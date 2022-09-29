0e559c501c764b474dcacd72e136f3a52b7b3b7f
Borys Mch <borys.mch@gmail.com>
2022-09-29 20:22:02 +0300

    Add section - Gallery

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { NavBar } from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Banner } from './components/Banner';
 import { Slider } from './components/Slider';
+import { Gallery } from './components/Gallery';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <NavBar />
       <Banner />
       <Slider />
+      <Gallery />
     </div>
   );
 }
---
+++ src/components/Gallery.js
@@ -0,0 +1,90 @@
+import { Col, Container, Nav, Row, Tab } from "react-bootstrap";
+import { GalleryCard } from "./GalleryCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import gallImg1 from "../assets/img/project-img1.png";
+import gallImg2 from "../assets/img/project-img2.png";
+import gallImg3 from "../assets/img/project-img3.png";
+
+export const Gallery = () => {
+
+    const galleries = [
+        {
+            title: "Business Startup",
+            description: "Designe & Development",
+            imgUrl: gallImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Designe & Development",
+            imgUrl: gallImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Designe & Development",
+            imgUrl: gallImg3,
+        },
+        {
+            title: "Business Startup",
+            description: "Designe & Development",
+            imgUrl: gallImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Designe & Development",
+            imgUrl: gallImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Designe & Development",
+            imgUrl: gallImg3,
+        },
+    ];
+    return (
+        <section className="gallery" id="gallery">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Gallery</h2>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        </p>
+                        <Tab.Container id="gallery-tabs" defaultActiveKey="first">
+                            <Nav variant="pills" defaultActiveKey="/home">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab One</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <Row>
+                                        {
+                                            galleries.map((gallery, index) => {
+                                                return (
+                                                    <GalleryCard 
+                                                    key={index}
+                                                    {...gallery}
+                                                    />
+                                                )
+                                            })
+                                        }
+                                    </Row>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">Loren Ipsum</Tab.Pane>
+                                <Tab.Pane eventKey="third">Loren Ipsum</Tab.Pane>
+                            </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2} />
+        </section>
+    );
+};
